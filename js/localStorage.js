5d76d00852d5cba8351e03d76e7e80be6c3da661
Felix Sopelsa <felix.sopelsa@gmail.com>
2021-06-14 16:13:39 +0200

    testar med allt i samma dokument

--- js/localStorage.js
+++ js/localStorage.js
@@ -3,30 +3,3 @@ function init() {
 	countGames = document.getElementById("countGames");
 } // End init
 window.addEventListener("load",init);
-// ------------------------------
-// Räknar ut summan av de poäng som sparats i arrayen totalPoints.
-function getSum(total, num) {
-    return total + Math.round(num);
-} // End getSum
-// ------------------------------
-// Sparar total poäng i localStorage
- function savePoints() {
-	let newTotalPoints = totalPoints.reduce(getSum, 0) // Summan av totalPoints
-	console.log(newTotalPoints,"newtotalpoints")
-	localStorage.fn222hnPoints = Number(newTotalPoints);
-    writeOutPointsAndGames();
-} // End savePointsAndGames
-// ------------------------------
-// Skriver ut localStorag 
-function writeOutPointsAndGames() {
-	console.log(localStorage.fn222hnGames,"localStorage.fn222hnGames")
-	console.log(localStorage.fn222hnPoints,"localStorage.fn222hnPoints")
-	if (localStorage.fn222hnPoints) {
-        totPoints.innerHTML = Number(localStorage.fn222hnPoints);
-	}
-    if (localStorage.fn222hnGames) {
-		countGames.innerHTML = Number(localStorage.fn222hnGames);
-	  }
-	  else
-	  	countGames.innerHTML =  "0";
-} // End writeOutPointsAndGames
--- js/script.js
+++ js/script.js
@@ -31,6 +31,12 @@ function initGame() {
     // Aktivera/inaktivera knappar
     newBricksBtn.disabled = true;
     newGameBtn.disabled = false;
+
+    
+	totPoints = document.getElementById("totPoints");
+	countGames = document.getElementById("countGames");
+
+    
 } // End init
 window.addEventListener("load",initGame); // Se till att init aktiveras då sidan är inladdad
 // ------------------------------
@@ -182,3 +188,30 @@ function showPoints() {
     newGameBtn.disabled = false;
     savePoints();
 } // End showPoints
+// ------------------------------
+// Räknar ut summan av de poäng som sparats i arrayen totalPoints.
+function getSum(total, num) {
+    return total + Math.round(num);
+} // End getSum
+// ------------------------------
+// Sparar total poäng i localStorage
+ function savePoints() {
+	let newTotalPoints = totalPoints.reduce(getSum, 0) // Summan av totalPoints
+	console.log(newTotalPoints,"newtotalpoints")
+	localStorage.fn222hnPoints = Number(newTotalPoints);
+    writeOutPointsAndGames();
+} // End savePointsAndGames
+// ------------------------------
+// Skriver ut localStorag 
+function writeOutPointsAndGames() {
+	console.log(localStorage.fn222hnGames,"localStorage.fn222hnGames")
+	console.log(localStorage.fn222hnPoints,"localStorage.fn222hnPoints")
+	if (localStorage.fn222hnPoints) {
+        totPoints.innerHTML = Number(localStorage.fn222hnPoints);
+	}
+    if (localStorage.fn222hnGames) {
+		countGames.innerHTML = Number(localStorage.fn222hnGames);
+	  }
+	  else
+	  	countGames.innerHTML =  "0";
+} // End writeOutPointsAndGames
